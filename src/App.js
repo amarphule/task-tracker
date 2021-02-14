fbf3c89049407c9a4d9ce938e70ccb005d4348c9
Amar Phule <amardeepphule@gmail.com>
2021-02-14 09:33:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Meeting with Sarang",
+      day: 'Feb 20th at 3pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'HR Call',
+      day: 'Feb 18th at 10am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Visit Collage',
+      day: 'Mar 1st at 10',
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: 'Food SHooping',
+      day: 'Feb 15th at 5pm',
+      reminder: false,
+    }
+  ])
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => (task.id !== id)))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task Tracker" />
+      <AddTask />
+      {tasks.length == 0 ? (
+        "No tasks to show") : (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)}
     </div>
   );
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,21 @@
+const AddTask = () => {
+    return (
+        <form className='add-form'>
+            <div className='form-control'>
+                <label>Add Task</label>
+                <input type='text' placeholder='Add task' />
+            </div>
+            <div className='form-control'>
+                <label>Day and Time</label>
+                <input type='text' placeholder='Day and Time' />
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Reminder</label>
+                <input type='checkbox' />
+            </div>
+            <input type='submit' value='Add Task' className='btn btn-block' />
+        </form>
+    )
+}
+
+export default AddTask
